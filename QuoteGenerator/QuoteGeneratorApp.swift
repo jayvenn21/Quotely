0f7e0fcf4ba7065eb469ab52399abb5b8da0d9c2
jayanth <jvennamreddy@gmail.com>
2024-04-06 21:47:19 -0400

    Initial Commit

---
+++ QuoteGenerator/QuoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteGeneratorApp.swift
+//  QuoteGenerator
+//
+//  Created by Jayanth on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
