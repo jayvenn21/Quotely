6317a0be0c111a8bec2f846d721f7e71a8448270
jayvenn21 <jvennamreddy@gmail.com>
2024-04-11 20:23:24 -0400

    Light mode dark mode toggle for home page

--- QuoteGenerator/ContentView.swift
+++ QuoteGenerator/ContentView.swift
@@ -5,28 +5,29 @@ struct ContentView: View {
     @State private var touchedWordIndex: Int?
 
     let hoverWords = [
-            "Inspiration", "Creativity", "Motivation", "Wisdom", "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope",
-            "Believe", "Adventure", "Persistence", "Ambition", "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism",
-            "Kindness", "Freedom", "Unity", "Resilience", "Peace", "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence",
-            "Harmony", "Kindness", "Transformation", "Simplicity", "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection",
-            "Connection", "Curiosity", "Generosity", "Empathy", "Awareness", "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom",
-            "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope", "Believe", "Adventure", "Persistence", "Ambition",
-            "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism", "Kindness", "Freedom", "Unity", "Resilience", "Peace",
-            "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence", "Harmony", "Kindness", "Transformation", "Simplicity",
-            "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection", "Connection", "Curiosity", "Generosity", "Empathy", "Awareness",
-            "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom", "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope",
-            "Believe", "Adventure", "Persistence", "Ambition", "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism",
-            "Kindness", "Freedom", "Unity", "Resilience", "Peace", "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence",
-            "Harmony", "Kindness", "Transformation", "Simplicity", "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection",
-            "Connection", "Curiosity", "Generosity", "Empathy", "Awareness", "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom",
-            "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope", "Believe", "Adventure", "Persistence", "Ambition",
-            "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism", "Kindness", "Freedom", "Unity", "Resilience", "Peace",
-            "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence", "Harmony", "Kindness", "Transformation", "Simplicity",
-            "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection", "Connection", "Curiosity", "Generosity", "Empathy", "Awareness",
-            "Laughter"
-        ]
-    
+        "Inspiration", "Creativity", "Motivation", "Wisdom", "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope",
+        "Believe", "Adventure", "Persistence", "Ambition", "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism",
+        "Kindness", "Freedom", "Unity", "Resilience", "Peace", "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence",
+        "Harmony", "Kindness", "Transformation", "Simplicity", "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection",
+        "Connection", "Curiosity", "Generosity", "Empathy", "Awareness", "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom",
+        "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope", "Believe", "Adventure", "Persistence", "Ambition",
+        "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism", "Kindness", "Freedom", "Unity", "Resilience", "Peace",
+        "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence", "Harmony", "Kindness", "Transformation", "Simplicity",
+        "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection", "Connection", "Curiosity", "Generosity", "Empathy", "Awareness",
+        "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom", "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope",
+        "Believe", "Adventure", "Persistence", "Ambition", "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism",
+        "Kindness", "Freedom", "Unity", "Resilience", "Peace", "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence",
+        "Harmony", "Kindness", "Transformation", "Simplicity", "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection",
+        "Connection", "Curiosity", "Generosity", "Empathy", "Awareness", "Laughter", "Inspiration", "Creativity", "Motivation", "Wisdom",
+        "Imagination", "Innovation", "Positivity", "Dreams", "Courage", "Hope", "Believe", "Adventure", "Persistence", "Ambition",
+        "Empowerment", "Gratitude", "Happiness", "Success", "Passion", "Optimism", "Kindness", "Freedom", "Unity", "Resilience", "Peace",
+        "Love", "Joy", "Discovery", "Growth", "Balance", "Purpose", "Confidence", "Harmony", "Kindness", "Transformation", "Simplicity",
+        "Gratitude", "Vision", "Strength", "Faith", "Progress", "Reflection", "Connection", "Curiosity", "Generosity", "Empathy", "Awareness",
+        "Laughter"
+    ]
+
     @State private var wordPositions: [String: CGPoint] = [:]
+    @State private var isLightMode = true // State variable for light/dark mode
 
     init() {
         // Initialize wordPositions with random positions within the screen bounds for each word
@@ -41,24 +42,36 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 // Background with moving objects
-                Color(UIColor(red: 245/255, green: 245/255, blue: 220/255, alpha: 1.0)) // Beige background color
-                
+                Color(UIColor(red: isLightMode ? 0.95 : 0.1, green: isLightMode ? 0.95 : 0.1, blue: isLightMode ? 0.95 : 0.1, alpha: 1.0)) // Grayish background color
+
                 VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            isLightMode.toggle()
+                        }) {
+                            Image(systemName: isLightMode ? "moon.fill" : "sun.max.fill")
+                                .font(.system(size: 24))
+                                .padding()
+                                .foregroundColor(isLightMode ? .black : .white) // Set color based on light/dark mode
+                        }
+                    }
+                    
                     Spacer() // Pushes the text to the middle
                     // Surrounding background for title text and button
                     VStack {
                         Text("Quotely")
                             .font(.system(size: 48, weight: .bold)) // Apple San Francisco font for the title
-                            .foregroundColor(.blue) // Blue color for the title text
+                            .foregroundColor(isLightMode ? .black : .white) // Text color based on light/dark mode
                             .padding()
                             .scaleEffect(isButtonTapped ? 1.2 : 1) // Scale animation when button is tapped
                             .animation(.easeInOut(duration: 0.3), value: isButtonTapped) // Animation modifier with value
-                        
+
                         NavigationLink(destination: HomePage(), isActive: $isButtonTapped) {
                             EmptyView()
                         }
                         .hidden() // Hide the navigation link label
-                        
+
                         Button(action: {
                             withAnimation {
                                 isButtonTapped.toggle()
@@ -66,7 +79,7 @@ struct ContentView: View {
                         }) {
                             Text("Open Quotely")
                                 .font(.system(size: 24)) // Apple San Francisco font for the button
-                                .foregroundColor(.green) // Green color for the button text
+                                .foregroundColor(isLightMode ? .green : .blue) // Button text color based on light/dark mode
                                 .padding()
                                 .background(Color.white)
                                 .cornerRadius(10) // Rounded button corners
@@ -74,18 +87,18 @@ struct ContentView: View {
                                 .scaleEffect(isButtonTapped ? 0.95 : 1) // Scale animation when button is tapped
                         }
                     }
-                    .background(Color(UIColor(red: 230/255, green: 230/255, blue: 200/255, alpha: 1.0))) // Darker beige background color
+                    .background(Color(UIColor(red: isLightMode ? 0.9 : 0.2, green: isLightMode ? 0.9 : 0.2, blue: isLightMode ? 0.9 : 0.2, alpha: 1.0))) // Darker gray background color
                     .gesture(
                         DragGesture().onChanged { _ in
                             // Handle the touch or pointer movement here
                             touchedWordIndex = nil // Reset the touched word index
                         }
                     )
-                    
+
                     Spacer() // Pushes the button to the middle
                     Spacer() // Add spacer to extend background to bottom of screen
                 }
-                
+
                 // Overlay with hovering words
                 ZStack {
                     ForEach(hoverWords, id: \.self) { word in
@@ -110,9 +123,10 @@ struct ContentView: View {
                     }
                 }
             }
-            .background(Color(UIColor(red: 230/255, green: 230/255, blue: 200/255, alpha: 1.0))) // Darker beige background color
+            .background(Color(UIColor(red: isLightMode ? 0.9 : 0.2, green: isLightMode ? 0.9 : 0.2, blue: isLightMode ? 0.9 : 0.2, alpha: 1.0))) // Darker gray background color
             .navigationBarHidden(true) // Hide the navigation bar
         }
+        .preferredColorScheme(isLightMode ? .light : .dark) // Toggle between light and dark mode
     }
 }
 
@@ -134,7 +148,7 @@ struct HoveringWordView: View {
                 self.position = CGPoint(x: randomX, y: randomY)
             }
     }
-    
+
     func randomColor() -> Color {
         let red = Double.random(in: 0..<1)
         let green = Double.random(in: 0..<1)
